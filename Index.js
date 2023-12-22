e82e3d1063038ea018b2733492af204ca3ae7874
sanjeev <sanjeev2.eminence@gmail.com>
2023-12-22 12:52:15 +0530

    FEAT - : - Data base connection with Postgr

--- Index.js
+++ Index.js
@@ -1,26 +1,23 @@
 const express =require('express')
 const cors=require('cors')
-
+const {updateData} =require('./database/connection')
+const { Pool } = require('pg');
 const app=express()
 const dotenv=require('dotenv')
 
 //const errorMiddleware=require('./Middleware/Error')
 dotenv.config()
 
+ 
+
+async function main(){
+   await updateData();
+ 
+}
+main();
 //Middlewares
 app.use(express.json())
 app.use(cors())
-
-//Routes 
-
-
-
-
-
-//Error Middleware
-
-//app.use(errorMiddleware())
-
 
 const port=process.env.PORT
 app.listen(()=>console.log(`server is running  http://localhost:${port}`))
