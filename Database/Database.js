96b68b8db23998847e3440eb7bab121b94969ef5
sanjeev <sanjeev2.eminence@gmail.com>
2023-12-29 10:15:25 +0530

    Fix: -_- added some comments

--- Database/Database.js
+++ Database/Database.js
@@ -1,5 +1,6 @@
 const mongoose=require('mongoose')
 const url=require('../Config')
+// mongo db url
 const Url=`mongodb+srv://aryantrivedieminence:feXnQRMDLiXUslAN@ecommercestore.87yxwpa.mongodb.net/?retryWrites=true&w=majority`;
 const Connection=async()=>{
   try {
